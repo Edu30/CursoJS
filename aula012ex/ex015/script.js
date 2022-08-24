f5c954f3e5b66837a06c757056d45f653c7ba653
Edu30 <53379289+Edu30@users.noreply.github.com>
2022-08-23 22:16:46 -0300

    ex015 2.0

--- aula012ex/ex015/script.js
+++ aula012ex/ex015/script.js
@@ -9,10 +9,37 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.getElementById('img')
         if (fsex[0].checked) {
             gênero = 'Homem'
+            if (idade >= 0 && idade <10) {
+                //criança
+                img.setAttribute('src', 'h-criança.jpg')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'h-jovem.jpg')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'h-adulto.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'h-idoso.jpg')
+            }
         } else {
             gênero = 'Mulher'
+            if (idade >= 0 && idade <10) {
+                //criança
+                img.setAttribute('src', 'f-criança.jpg')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'f-jovem.jpg')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'f-adulta.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'f-idosa.jpg')
+            }
         }
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
     }
