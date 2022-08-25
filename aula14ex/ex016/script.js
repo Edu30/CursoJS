f1b695972adaa594e89c35d543e0071448ad87cf
Edu30 <53379289+Edu30@users.noreply.github.com>
2022-08-25 16:59:48 -0300

    ex016

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,7 +1,34 @@
-var inicio = '1'
-var fim = '5'
-var passos = '1'
-var c = ''
-for (var c = inicio; c <= fim; c = c + passos) {
-    console.log(c)
+function contar() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+        //alert('[ERRO] Faltam dados!')
+        
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem crescente
+            for (var c=i; c<=f; c+=p) {
+                res.innerHTML += `\u{1F449} ${c} `
+            }
+            
+        } else {
+            //contagem decrescente
+            for (var c=i; c>=f; c-=p) {
+                res.innerHTML += `\u{1F449} ${c} `
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
